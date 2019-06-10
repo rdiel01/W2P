79a1a8f34e7cc9f33719925923df54cb08e1f1f3
Ryan Diel <rdiel01@gmail.com>
2019-06-10 15:50:48 -0500

    add class for handling price table actions

---
+++ src/OrderForge/automation/tools/PriceTable.java
@@ -0,0 +1,38 @@
+package OrderForge.automation.tools;
+
+import OrderForge.automation.*;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class PriceTable {
+
+    private static WebDriver driver = null;
+
+    private static WebElement element = null;
+
+    public static WebElement btn_add_unit_break(WebDriver driver) {
+
+        element = driver.findElement(By.xpath("/html/body/section/div[2]/div[1]/div[1]/a[3]"));
+
+        return element;
+    }
+
+    public static WebElement txtbx_add_unit_break(WebDriver driver) {
+        element = driver.findElement(By.xpath("/html/body/div[3]/div[2]/p[3]/input"));
+
+        return element;
+    }
+
+    public static WebElement btn_submit_add_unit_break(WebDriver driver) {
+
+        element = driver.findElement(By.xpath("/html/body/div[3]/div[2]/p[4]/a[2]"));
+
+        return element;
+    }
+
+}
