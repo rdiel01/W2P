b90563a2f6cfac6b7da228304b71c3d25f6f498b
Ryan Diel <rdiel01@gmail.com>
2019-08-16 11:52:07 -0500

    remove imports

--- src/OrderForge/automation/tools/PriceTable.java
+++ src/OrderForge/automation/tools/PriceTable.java
@@ -1,13 +1,9 @@
 package OrderForge.automation.tools;
 
-import OrderForge.automation.*;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-
-import java.util.concurrent.TimeUnit;
 
 public class PriceTable {
 
